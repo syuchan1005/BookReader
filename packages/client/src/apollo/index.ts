82b111e54744c4fb8aa1b7630043ef15b9a881cc
syuchan1005 <syuchan.dev@gmail.com>
2021-06-16 20:33:43 +0900

    split create custom fetcher

--- packages/client/src/apollo/index.ts
+++ packages/client/src/apollo/index.ts
@@ -2,68 +2,9 @@ import { ApolloClient, ApolloLink, InMemoryCache } from '@apollo/client';
 import { WebSocketLink } from '@apollo/client/link/ws';
 import { createUploadLink } from 'apollo-upload-client';
 import { getMainDefinition } from 'apollo-utilities';
+import createCustomFetcher from '@client/CustomFetcher';
 
 const uri = `//${window.location.hostname}:${window.location.port}/graphql`;
-
-const parseHeaders = (rawHeaders: any) => {
-  const headers = new Headers();
-  // Replace instances of \r\n and \n followed by at least one space or horizontal tab with a space
-  // https://tools.ietf.org/html/rfc7230#section-3.2
-  const preProcessedHeaders = rawHeaders.replace(/\r?\n[\t ]+/g, ' ');
-  preProcessedHeaders.split(/\r?\n/).forEach((line: any) => {
-    const parts = line.split(':');
-    const key = parts.shift().trim();
-    if (key) {
-      const value = parts.join(':').trim();
-      headers.append(key, value);
-    }
-  });
-  return headers;
-};
-
-const uploadFetch = (url: string, options: any) => new Promise((resolve, reject) => {
-  const xhr = new XMLHttpRequest();
-  xhr.onload = () => {
-    const opts: any = {
-      status: xhr.status,
-      statusText: xhr.statusText,
-      headers: parseHeaders(xhr.getAllResponseHeaders() || ''),
-    };
-    opts.url = 'responseURL' in xhr
-      ? xhr.responseURL
-      : opts.headers.get('X-Request-URL');
-    const body = 'response' in xhr ? xhr.response : (xhr as any).responseText;
-    resolve(new Response(body, opts));
-  };
-  xhr.onerror = () => {
-    reject(new TypeError('Network request failed'));
-  };
-  xhr.ontimeout = () => {
-    reject(new TypeError('Network request timeout'));
-  };
-  xhr.open(options.method, url, true);
-
-  Object.keys(options.headers).forEach((key) => {
-    xhr.setRequestHeader(key, options.headers[key]);
-  });
-
-  if (xhr.upload) {
-    xhr.upload.onprogress = options.onProgress;
-  }
-
-  options.onAbortPossible(() => {
-    xhr.abort();
-  });
-
-  xhr.send(options.body);
-});
-
-const customFetch = (uri1: any, options: any) => {
-  if (options.useUpload) {
-    return uploadFetch(uri1, options);
-  }
-  return fetch(uri1, options);
-};
 
 const getClient = async (): Promise<ApolloClient<any>> => {
   const apolloClient = new ApolloClient({
@@ -85,7 +26,13 @@ const getClient = async (): Promise<ApolloClient<any>> => {
       createUploadLink({
         uri: `${window.location.protocol}${uri}`,
         // credentials: "same-origin",
-        fetch: customFetch as any,
+        fetch: (url, options) => {
+          let f = fetch;
+          if (options.useUpload) {
+            f = createCustomFetcher(options.onProgress, options.onAbortPossible);
+          }
+          return f(url, options);
+        },
       }),
     ).setOnError(({ graphQLErrors, networkError }) => {
       const log = (message) => {
