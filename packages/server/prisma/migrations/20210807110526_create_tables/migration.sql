e42a604991049f7dd5b5931c391748273cbbb230
syuchan1005 <syuchan.dev@gmail.com>
2021-08-23 21:59:24 +0900

    fix indent

--- packages/server/prisma/migrations/20210807110526_create_tables/migration.sql
+++ packages/server/prisma/migrations/20210807110526_create_tables/migration.sql
@@ -1,40 +1,40 @@
 -- CreateTable
 CREATE TABLE "BookInfo" (
-                            "id" TEXT NOT NULL PRIMARY KEY,
-                            "name" TEXT NOT NULL,
-                            "historyBookCount" INTEGER,
-                            "createdAt" DATETIME NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')),
-                            "updatedAt" DATETIME NOT NULL
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "historyBookCount" INTEGER,
+    "createdAt" DATETIME NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')),
+    "updatedAt" DATETIME NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "Book" (
-                        "id" TEXT NOT NULL PRIMARY KEY,
-                        "infoId" TEXT NOT NULL,
-                        "thumbnailPage" INTEGER,
-                        "number" TEXT NOT NULL,
-                        "pageCount" INTEGER NOT NULL,
-                        "thumbnailById" TEXT,
-                        "createdAt" DATETIME NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')),
-                        "updatedAt" DATETIME NOT NULL,
-                        FOREIGN KEY ("infoId") REFERENCES "BookInfo" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-                        FOREIGN KEY ("thumbnailById") REFERENCES "BookInfo" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "infoId" TEXT NOT NULL,
+    "thumbnailPage" INTEGER,
+    "number" TEXT NOT NULL,
+    "pageCount" INTEGER NOT NULL,
+    "thumbnailById" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')),
+    "updatedAt" DATETIME NOT NULL,
+    FOREIGN KEY ("infoId") REFERENCES "BookInfo" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY ("thumbnailById") REFERENCES "BookInfo" ("id") ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 -- CreateTable
 CREATE TABLE "Genre" (
-                         "name" TEXT NOT NULL PRIMARY KEY,
-                         "isInvisible" BOOLEAN NOT NULL DEFAULT false
+    "name" TEXT NOT NULL PRIMARY KEY,
+    "isInvisible" BOOLEAN NOT NULL DEFAULT false
 );
 
 -- CreateTable
 CREATE TABLE "BookInfosToGenres" (
-                                     "infoId" TEXT NOT NULL,
-                                     "genreName" TEXT NOT NULL,
+    "infoId" TEXT NOT NULL,
+    "genreName" TEXT NOT NULL,
 
-                                     PRIMARY KEY ("infoId", "genreName"),
-                                     FOREIGN KEY ("infoId") REFERENCES "BookInfo" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-                                     FOREIGN KEY ("genreName") REFERENCES "Genre" ("name") ON DELETE CASCADE ON UPDATE CASCADE
+    PRIMARY KEY ("infoId", "genreName"),
+    FOREIGN KEY ("infoId") REFERENCES "BookInfo" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY ("genreName") REFERENCES "Genre" ("name") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateIndex
@@ -55,7 +55,7 @@ CREATE UNIQUE INDEX "Book_infoId_number_unique" ON "Book"("infoId", "number");
 -- CreateIndex
 CREATE UNIQUE INDEX "Book_thumbnailById_unique" ON "Book"("thumbnailById");
 
-
 -- Seeding (It's a workaround for windows problem in seed feature)
 INSERT INTO Genre (name, isInvisible) VALUES ('Invisible', true);
+
 INSERT INTO Genre (name, isInvisible) VALUES ('Completed', false);
