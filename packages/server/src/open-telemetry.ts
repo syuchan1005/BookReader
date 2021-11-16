d51952a8c0ab94b0fa74a2f767b43ca9e712379c
syuchan1005 <syuchan.dev@gmail.com>
2021-11-16 21:33:46 +0900

    Add Span supporting functions

--- packages/server/src/open-telemetry.ts
+++ packages/server/src/open-telemetry.ts
@@ -7,6 +7,31 @@ import { KoaInstrumentation } from '@opentelemetry/instrumentation-koa';
 import { GraphQLInstrumentation } from '@opentelemetry/instrumentation-graphql';
 import { ZipkinExporter } from '@opentelemetry/exporter-zipkin';
 
+import opentelemetry, { Span as OTSpan } from '@opentelemetry/api';
+import { OTEL_GRAPHQL_DATA_SYMBOL } from '@opentelemetry/instrumentation-graphql/build/src/symbols';
+
+const serviceName = 'book-reader';
+
+export type Span = OTSpan;
+type Context = unknown;
+const tracer = opentelemetry.trace.getTracer(serviceName);
+const getSpan = (context: Context): Span | undefined => context?.[OTEL_GRAPHQL_DATA_SYMBOL]?.span;
+
+export const startSpan = (parentSpan: Span, spanName: Lowercase<string>): Span => {
+  const ctx = opentelemetry.trace.setSpan(opentelemetry.context.active(), parentSpan);
+  return tracer.startSpan(spanName, undefined, ctx);
+};
+export const startSpanFromContext = (
+  context: Context,
+  spanName: Lowercase<string>,
+): Span | undefined => {
+  const parentSpan = getSpan(context);
+  if (!parentSpan) {
+    return undefined;
+  }
+  return startSpan(parentSpan, spanName);
+};
+
 const zipkinEndpoint = process.env.ZIPKIN_ENDPOINT;
 if (zipkinEndpoint) {
   registerInstrumentations({
@@ -20,7 +45,7 @@ if (zipkinEndpoint) {
   const provider = new NodeTracerProvider({
     resource: Resource.default()
       .merge(new Resource({
-        'service.name': 'book-reader',
+        'service.name': serviceName,
       })),
   });
 
