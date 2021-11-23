5a28c67106ca884843b3fdebd83eda1416aad1ef
syuchan1005 <syuchan.dev@gmail.com>
2021-11-23 14:16:33 +0900

    Split chunks for some big npm packages

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -38,6 +38,26 @@ export default defineConfig({
   },
   build: {
     brotliSize: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-router-dom', 'react-dom'],
+          apollo: [
+            'graphql',
+            '@apollo/client',
+            'apollo-upload-client',
+            'apollo3-cache-persist',
+            'subscriptions-transport-ws',
+          ],
+          mui: [
+            '@emotion/react',
+            '@emotion/styled',
+            '@mui/material',
+            '@mui/styles',
+          ],
+        },
+      },
+    },
   },
   plugins: [
     react(),
