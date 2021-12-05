09c4ab8199af89b529e13cff767255cf0da415f2
syuchan1005 <syuchan.dev@gmail.com>
2021-12-05 12:24:34 +0900

    Make vite time plugin

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,8 +1,9 @@
+/* eslint-disable no-console */
 import { resolve } from 'path';
 import fs from 'fs';
 
 import { defineConfig, Plugin } from 'vite';
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 import bundleVisualizer from 'rollup-plugin-visualizer';
 
@@ -22,6 +23,46 @@ const RemoveServiceWorkerTsFilePlugin = (): Plugin => {
           console.log(`Remove: ${outDir}/${serviceWorkerFileName}`);
         } catch (ignored) {
           /* ignored */
+        }
+      }
+    },
+  };
+};
+
+const TimePlugin = (): Plugin => {
+  const startTime = Date.now();
+
+  let buildStartTime: number;
+  let buildEndTime: number;
+  let renderStartTime: number;
+
+  const period = (s: number, e: number) => (Math.abs((e - s)) / 1000).toFixed(3);
+
+  return {
+    name: 'vite-plugin-build-time',
+    apply: 'build',
+    buildStart() {
+      buildStartTime = Date.now();
+    },
+    buildEnd() {
+      buildEndTime = Date.now();
+    },
+    renderStart() {
+      renderStartTime = Date.now();
+    },
+    renderError() {
+      renderStartTime = Date.now();
+    },
+    closeBundle() {
+      const endTime = Date.now();
+      // Catch potential render NPEs
+      try {
+        console.log(`build  ${period(buildStartTime, buildEndTime)}s`);
+        console.log(`render ${period(renderStartTime, endTime)}s`);
+        console.log(`full   ${period(startTime, endTime)}s`);
+      } catch (error) {
+        if (error instanceof Error) {
+          this.warn(error);
         }
       }
     },
@@ -73,6 +114,7 @@ export default defineConfig({
       gzipSize: true,
       filename: 'dist/stats.html',
     }),
+    TimePlugin(),
   ],
   server: {
     port: 8080,
