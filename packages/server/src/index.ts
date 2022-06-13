0a0c13b1c0b94cba6ec7cc8528dc834da083cbd9
syuchan1005 <syuchan.dev@gmail.com>
2022-06-13 20:03:04 +0900

    Add access log export

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,5 +1,6 @@
 import http from 'http';
 import express from 'express';
+import morgan from 'morgan';
 import history from 'connect-history-api-fallback';
 import cors from 'cors';
 
@@ -14,6 +15,34 @@ import GraphQL from './graphql/index';
   const app = express();
   const httpServer = http.createServer(app);
   const graphql = new GraphQL(httpServer);
+
+  app.use(morgan((tokens, req, res) => {
+    let severity = 'INFO';
+    const status = tokens.status(req, res);
+
+    if (status >= 500) severity = 'ERROR';
+    else if (status >= 400) severity = 'WARNING';
+
+    const httpRequest = {
+      requestMethod: tokens.method(req, res),
+      requestUrl: tokens.url(req, res),
+      status,
+      responseSize: tokens.res(req, res, 'content-length'),
+      userAgent: tokens['user-agent'](req, res),
+      remoteIp: tokens['remote-addr'](req, res),
+      referer: tokens.referrer(req, res),
+      protocol: `HTTP/${tokens['http-version'](req, res)}`,
+    };
+
+    return JSON.stringify({
+      severity,
+      httpRequest,
+      remote_user: tokens['remote-user'](req, res),
+      timestamp: tokens.date(req, res, 'iso'),
+      response_time: tokens['response-time'](req, res),
+      total_time: tokens['total-time'](req, res),
+    });
+  }));
 
   app.use(cors());
 
