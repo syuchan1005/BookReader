7358ff01efbe4936bb336b3f5e6206b954d295a2
syuchan1005 <syuchan.dev@gmail.com>
2026-06-12 10:51:05 +0900

    fix: show book page number overlay

--- packages/client/src/components/BookPageOverlay.tsx
+++ packages/client/src/components/BookPageOverlay.tsx
@@ -57,7 +57,7 @@ const Root = styled('div')(({ theme }) => ({
     borderRadius: theme.spacing(1),
     position: 'absolute',
     '&.top': {
-      top: `calc(${commonTheme.safeArea.top} + ${theme.spacing(2)})`,
+      top: `calc(${commonTheme.safeArea.top} + ${theme.spacing(12)})`,
       whiteSpace: 'nowrap',
     },
     '&.bottom': {
